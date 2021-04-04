5e57f58815306a3b6939b23fe93687366aafb6ee
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2021-04-04 15:49:52 +0600

    [CSE 310] Errors completed.. debugging

--- CSE 310/Offline2/src/run.sh
+++ CSE 310/Offline2/src/run.sh
@@ -1,3 +1,14 @@
 flex 1705066.l
 g++ lex.yy.c -lfl
-./a.out $1
+
+command=$1
+
+if [ "$command" = "clr" ]
+then 
+    rm -rf 1705066_log.txt
+    rm -rf 1705066_token.txt
+    rm -rf a.out
+    rm -rf lex.yy.c
+else
+    ./a.out "$command"
+fi
