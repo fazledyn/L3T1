8e29e408e25ac5769d6dfacfccc31fbf030d8173
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2021-04-20 06:36:42 +0600

    Some utils function added

--- CSE 310/Offline3/src/lib/util.h
+++ CSE 310/Offline3/src/lib/util.h
@@ -12,3 +12,46 @@ void printLog(FILE* file, string rule, string token, int lineCount)
 void printError(FILE* file, string errorText, int lineCount) {
     fprintf(file, "Error at line %d: %s\n\n", lineCount, errorText.c_str());
 }
+
+vector<string> splitString(string line) {
+
+    stringstream ss(line);
+    vector<string> tokens;
+    string intermediate;
+
+    while(getline(ss, intermediate, ',')) {
+        tokens.push_back(intermediate);
+    }
+    return tokens;
+}
+
+bool isArray(string line) {
+    if ((line.find("[") != string::npos) || (line.find("]") != string::npos)) {
+        return true;
+    }
+    return false;
+}
+
+string extractArrayName(string line) {
+    
+    stringstream ss(line);
+    string arrayName;
+
+    getline(ss, arrayName, '[');
+    return arrayName;
+}
+
+int extractArraySize(string line) {
+
+    stringstream ss(line);
+    string token;
+
+    while (getline(ss, token, '[')) {
+        // 
+    }
+
+    stringstream ss2(token);
+    getline(ss2, token, ']');
+
+    return stoi(token);
+}
