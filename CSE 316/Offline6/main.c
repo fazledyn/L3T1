097a3995a30f1fc2ed41b393f385f7706e233da9
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2021-06-03 04:10:44 +0600

    [CSE316] Flags for registers added. Yet to test.

--- CSE 316/Offline6/main.c
+++ CSE 316/Offline6/main.c
@@ -22,17 +22,41 @@
 
 int main(void)
 {
+    unsigned int adc_level = 0x0000;
+    float voltage;
+    char voltage_string[10];
+
 	DDRD = 0xFF;
 	DDRC = 0xFF;
+
+    /*
+        ADMUX   = REFS1 REFS0 ADLAR MUX(4-0)
+                  0     1     0     0 0 0 0 1
+        ADCSRA  = ADEN ADSC ADATE ADIF ADIE ADPS(2-0)
+                  1     ?     1     ?   ?     1 0 0
+    */
+    ADMUX  = 0b01000001;
+    ADCSRA = 0b10000100;
 
 	Lcd4_Init();
 	while(1)
 	{
+        // Start the conversion by putting the flag
+        ADCSRA |= (1 << ADSC);
+        // Wait for the conversion to over
+        while (ADCSRA & (1 << ADSC)) {}
+
+        adc_level |= ADCL;
+        adc_level |= (ADCH << 8);
+
+        voltage = (adc_level * 4.00)/16;
+        gcvt(voltage, 10, voltage_string);
+
 		Lcd4_Set_Cursor(1,1);
-		Lcd4_Write_String("Nahian Chan <3");
+		Lcd4_Write_String("Voltage:");
 		
 		Lcd4_Set_Cursor(2,1);
-		Lcd4_Write_String("Rabidoggo");
+		Lcd4_Write_String(voltage_string);
 		
 		_delay_ms(2000);
 	}
