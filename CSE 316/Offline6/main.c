7cae2895c9b7850da1cd7af33e9c8811a602c052
Ataf Fazledin Ahamed <1705066@ugrad.cse.buet.ac.bd>
2021-06-03 18:23:24 +0600

    [CSE316] Offline 6 completed

--- CSE 316/Offline6/main.c
+++ CSE 316/Offline6/main.c
@@ -17,47 +17,52 @@
 #define EN eS_PORTC7
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include "lcd.h" //Can be download from the bottom of this article
+#include <stdlib.h>
+
+#include "lcd.h"
+
+
+volatile uint16_t adc_total;
+
+ISR (ADC_vect) {
+	uint8_t low = ADCL;
+	adc_total = (ADCH << 8) | low;
+}
 
 int main(void)
 {
-    unsigned int adc_level = 0x0000;
-    float voltage;
-    char voltage_string[10];
-
 	DDRD = 0xFF;
 	DDRC = 0xFF;
-
     /*
         ADMUX   = REFS1 REFS0 ADLAR MUX(4-0)
-                  0     1     0     0 0 0 0 1
+                  0     0     0     0 0 0 0 1
         ADCSRA  = ADEN ADSC ADATE ADIF ADIE ADPS(2-0)
                   1     ?     1     ?   ?     1 0 0
     */
-    ADMUX  = 0b01000001;
-    ADCSRA = 0b10000100;
+    ADMUX  = 0b00000001;
+    ADCSRA = 0b10001100;
 
 	Lcd4_Init();
+	Lcd4_Set_Cursor(1,1);
+	Lcd4_Write_String("Voltage:");
+	
+	char voltage_string[10];
+	float adc_float;
+	sei();
+	
 	while(1)
 	{
-        // Start the conversion by putting the flag
-        ADCSRA |= (1 << ADSC);
-        // Wait for the conversion to over
-        while (ADCSRA & (1 << ADSC)) {}
-
-        adc_level |= ADCL;
-        adc_level |= (ADCH << 8);
-
-        voltage = (adc_level * 4.00)/16;
-        gcvt(voltage, 10, voltage_string);
-
-		Lcd4_Set_Cursor(1,1);
-		Lcd4_Write_String("Voltage:");
+		ADCSRA |= (1 << ADSC);				// Start conversion
+		while (ADCSRA & (1 << ADSC)) {}		// Wait for conversion to over
+		
+		adc_float = (adc_total * 4.00)/1024;
+		dtostrf(adc_float, 6, 4, voltage_string);
 		
 		Lcd4_Set_Cursor(2,1);
 		Lcd4_Write_String(voltage_string);
-		
-		_delay_ms(2000);
 	}
+	
+	return 0;
 }
